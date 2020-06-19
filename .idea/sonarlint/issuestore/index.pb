e5b3c7bf7c40622a5c86b0c3de8d23dc9c83b5b6
Kamika-ZE <krysnn@gmail.com>
2020-06-19 11:06:18 +0200

    ajout des tests unitaires classes du model

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+u
+Edocker/dev/init/db/docker-entrypoint-initdb.d/21_insert_data_demo.sql,f\0\f097df5295ff73aa2a0fe48a69123cee4b07eea2
--- src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
+++ src/myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/JournalComptableTest.java
@@ -1,4 +1,22 @@
 package com.dummy.myerp.model.bean.comptabilite;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class JournalComptableTest {
+
+    private JournalComptable journalComptable;
+
+    @Before
+    public void initJournalComptable(){
+        journalComptable = new JournalComptable();
+        journalComptable.setCode("AC");
+        journalComptable.setLibelle("Achat");
+    }
+
+    @Test
+    public void testJournalComptableToString(){
+        Assert.assertEquals("JournalComptable{code='AC', libelle='Achat'}", journalComptable.toString());
+    }
 }
