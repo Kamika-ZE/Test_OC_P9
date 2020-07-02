7b49427abf8aab219fe3fe4d53abe381d5919684
Kamika-ZE <krysnn@gmail.com>
2020-07-02 22:21:57 +0200

    ajout de tests. Test consumer non fonctionnel. Corrections diverses

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/ComptabiliteDaoImplIntegrationTest.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/ComptabiliteDaoImplIntegrationTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class ConsumerIntegrationTest extends ConsumerTestCase {
+public class ComptabiliteDaoImplIntegrationTest extends ConsumerTestCase {
 
     @Test
     public void getListCompteComptable() {
