eac1cd770240f4983fb2307457838323daf2da7d
Kamika-ZE <krysnn@gmail.com>
2020-07-03 17:46:52 +0200

    correction d'une erreur de frappe

--- src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/SpringRegistry.java
+++ src/myerp-consumer/src/test-consumer/java/com/dummy/myerp/testconsumer/consumer/SpringRegistry.java
@@ -29,7 +29,7 @@ public final class SpringRegistry {
      * Nom des fichiers de contexte de l'application
      */
     private static final String CONTEXT_APPLI_LOCATION
-            = "classpath:/com/dummy/myerp/testconsumer/consumer/TestContext.xml";
+            = "classpath:/com/dummy/myerp/testconsumer/consumer/testContext.xml";
 
     /**
      * Le context spring de l'application
